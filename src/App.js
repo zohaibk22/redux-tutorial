1e18e4e77d2f064d3722e4b7c748e6618b7f6953
Zohaib Khan <zohaibk1111@gmail.com>
2021-10-10 22:14:22 -0400

    made use of our state with the useSelector Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux'
 
 function App() {
+
+  const account = useSelector((state)=>{
+    console.log(state)
+    return state.account
+  })
+
+  console.log(account)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>{account}</h1>
+    <button>Deposit</button>
+    <button>withdraw</button>
+      
     </div>
   );
 }
--- src/state/store.js
+++ src/state/store.js
@@ -1,3 +1,8 @@
 import { createStore } from 'redux'
 //import your reducers... the combined reducers
 import reducers from "./reducers/index"
+
+export const store = createStore(
+    reducers,
+    {}
+)
